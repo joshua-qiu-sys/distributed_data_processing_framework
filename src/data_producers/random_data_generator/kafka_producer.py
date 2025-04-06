7ea303b9c53b83d856e899f6a48f804ad8f9c0d0
Joshua Qiu <joshua.qiu8@gmail.com>
2025-04-06 21:46:58 +1000

    Added avro and string serialisers/deserialisers to kafka producer when sending messages and receiving acks from delivery callbacks

--- src/data_producers/random_data_generator/kafka_producer.py
+++ src/data_producers/random_data_generator/kafka_producer.py
@@ -1,8 +1,14 @@
 from confluent_kafka import Producer, KafkaException
+from confluent_kafka.schema_registry.schema_registry_client import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer, AvroDeserializer
+from confluent_kafka.serialization import StringSerializer, StringDeserializer, SerializationContext, MessageField
+from typing import Dict
 from random import choice
 import time
+from decimal import Decimal
 import logging
 from read_kafka_producer_cfg import KafkaProducerCfgReader
+from schemas.consumer_good import ConsumerGood
 
 logger = logging.getLogger(f'random_data_generator')
 
@@ -16,19 +22,50 @@ def produce_message():
     producer.init_transactions()
     print(f'Initiated producer transactions')
 
+    def consumer_good_to_dict(consumer_good: ConsumerGood, ctx: SerializationContext = None) -> Dict:
+        consumer_good_dict = {
+            'item': consumer_good.item,
+            'retailer': consumer_good.retailer,
+            'price': consumer_good.price
+        }
+        return consumer_good_dict
+
+    schema_registry_conf = {
+        'url': 'http://localhost:8081'
+    }
+    schema_registry = SchemaRegistryClient(schema_registry_conf)
+    consumer_good_schema = schema_registry.get_latest_version(subject_name='ConsumerGood-value')
+    consumer_good_schema_str = consumer_good_schema.schema.schema_str
+    avro_serialiser = AvroSerializer(schema_registry_client=schema_registry, schema_str=consumer_good_schema_str, to_dict=consumer_good_to_dict)
+    string_serialiser = StringSerializer('utf8')
+
     def delivery_callback(err, msg):
+        def consumer_good_from_dict(consumer_good_dict: Dict, ctx: SerializationContext = None) -> ConsumerGood:
+            consumer_good = ConsumerGood(item=consumer_good_dict['price'],
+                                         retailer=consumer_good_dict['retailer'],
+                                         price=consumer_good_dict['price'])
+            return consumer_good
+        
         if err:
             print(f'ERROR: Message delivery failed: {err}')
         else:
             topic = msg.topic()
-            key = msg.key().decode('utf-8')
-            value = msg.value().decode('utf-8')
-            print(f'SUCCESS: Message delivery succeeded: {{"topic": {topic}, "key": {key}, "value": {value}}}')
+            
+            string_deserialiser = StringDeserializer('utf8')
+            key = string_deserialiser(msg.key())
+            avro_deserialiser = AvroDeserializer(schema_registry_client=schema_registry, schema_str=consumer_good_schema_str, from_dict=consumer_good_from_dict)
+            value = avro_deserialiser(msg.value(), SerializationContext(topic, MessageField.VALUE))
+            print(f'SUCCESS: Message delivery succeeded: {{"topic": {topic}, "key": {key}, "value": {consumer_good_to_dict(value)}}}')
 
     topic = 'uncatg_landing_zone'
     products = ['computer', 'television', 'smartphone', 'book', 'clothing', 'alarm clock', 'batteries', 'headphones',
                 'toothpaste', 'shampoo', 'laundry detergent', 'paper towels', 'charger', 'sunscreen', 'instant noodles',
                 'packaged snacks', 'light bulb', 'bottled water', 'air freshener', 'cooking oil', 'canned soup']
+    retailers = ['Woolworths', 'Coles', 'Aldi', 'IGA', 'Amazon', 'Costco']
+    prices = [Decimal('5.00'), Decimal('10.00'), Decimal('4.50'), Decimal('9.99'), Decimal('9.83'), Decimal('25.60'),
+              Decimal('35.40'), Decimal('23.87'), Decimal('15.00'), Decimal('12.30'), Decimal('2.01'), Decimal('7.45'),
+              Decimal('5.07'), Decimal('3.88'), Decimal('75.35'), Decimal('11.00'), Decimal('3.00'), Decimal('1.00'),
+              Decimal('9.90'), Decimal('78.39'), Decimal('2.00')]
 
     poll_interval = 3
     # flush_interval = 30
@@ -40,10 +77,19 @@ def produce_message():
     while True:
         try:
             producer.begin_transaction()
+
             key = str(products.index(choice(products)))
-            value = choice(products)
-            producer.produce(topic, value, key, callback=delivery_callback)
-            print(f'Sent data to buffer: {{"topic": {topic}, "key": {key}, "value": {value}}}')
+            consumer_good_item = choice(products)
+            consumer_good_retailer = choice(retailers)
+            consumer_good_price = choice(prices)
+            consumer_good = ConsumerGood(item=consumer_good_item, retailer=consumer_good_retailer, price=consumer_good_price)
+
+            producer.produce(topic=topic,
+                             key=string_serialiser(key),
+                             value=avro_serialiser(consumer_good, SerializationContext(topic, MessageField.VALUE)),
+                             callback=delivery_callback)
+            print(f'Sent data to buffer: {{"topic": {topic}, "key": {key}, "value": {consumer_good_to_dict(consumer_good)}}}')
+            
             producer.commit_transaction()
             print("Transaction committed successfully")
             print(f'Count: {count}')
